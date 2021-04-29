3047e28d04dc1ab15ff94953b1773342661935cf
Arash Saeedi <saeediarash84@gmail.com>
2021-04-29 17:47:03 -0400

    save point for class

--- employee-directory/src/components/Container/EmployeesContainer.jsx
+++ employee-directory/src/components/Container/EmployeesContainer.jsx
@@ -8,10 +8,10 @@ class EmployeeContainer extends Component {
         search: "",
         employees: [],
         searchFilters: [],
-        informationSort: this.sortingInfo,
+        informationSort: this.getSortingInfo ,
     };
 
-    sortingInfo = () => {
+    getSortingInfo()  {
         return {
             name: "",
             phone: "",
@@ -30,6 +30,32 @@ class EmployeeContainer extends Component {
                 })
             )
             .cathc((err) => console.log(err));
+    }
+
+    // Updating the state of search to filter by imput
+    handleInputChange = (e) => {
+        e.preventDefault();
+    };
+
+    // Using the key of the specified object to sort its info (and any children it had in the tree)
+    // sorting by primary and secondary child i.e last name, first name.
+    sortBy = (key, primary = 0, secondary = 0) => {
+        let sortedEmployees = this.state.searchFilters;
+        if (this.state.informationSort[key]) {
+            this.setState({
+                searchFilters: sortedEmployees.reverse(),
+                informationSort: {
+                    ...this.getSortingInfo,
+                    [key]: this.state.informationSort[key] === "asc" ? "desc" : "asc",
+                },
+            });
+        }else {
+            sortedEmployees = this.state.searchFilters.sort((a, b) => {
+                a = a[key];
+                b = b[key];
+
+            })
+        }
     }
 
     render() {
