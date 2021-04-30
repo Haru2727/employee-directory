bc9597b14049d630798b45e04fff8363ea6329ab
Arash Saeedi <saeediarash84@gmail.com>
2021-04-29 23:53:01 -0400

    finished the employee container code that will hold most of the functions

--- employee-directory/src/components/Container/EmployeesContainer.jsx
+++ employee-directory/src/components/Container/EmployeesContainer.jsx
@@ -8,10 +8,10 @@ class EmployeeContainer extends Component {
         search: "",
         employees: [],
         searchFilters: [],
-        informationSort: this.getSortingInfo ,
+        informationSort: this.getSortingInfo,
     };
 
-    getSortingInfo()  {
+    getSortingInfo() {
         return {
             name: "",
             phone: "",
@@ -32,11 +32,17 @@ class EmployeeContainer extends Component {
             .cathc((err) => console.log(err));
     }
 
-    // Updating the state of search to filter by imput
+    // Updating the state of search to filter by input
     handleInputChange = (e) => {
-        e.preventDefault();
+        const value = e.target.value;
+        this.setState({ search: value });
+        this.filterEmployees(value.toLowerCase().trim());
     };
 
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+    };
     // Using the key of the specified object to sort its info (and any children it had in the tree)
     // sorting by primary and secondary child i.e last name, first name.
     sortBy = (key, primary = 0, secondary = 0) => {
@@ -49,14 +55,14 @@ class EmployeeContainer extends Component {
                     [key]: this.state.informationSort[key] === "asc" ? "desc" : "asc",
                 },
             });
-        }else {
+        } else {
             sortedEmployees = this.state.searchFilters.sort((a, b) => {
                 a = a[key];
                 b = b[key];
-                
+
                 // if the search for given input is equal to its comparison then it will sort 
                 // the search with the other child onject
-                if(primary) {
+                if (primary) {
                     if (secondary && a[primary] === b[primary]) {
                         return a[secondary].localeCompare(b[secondary]);
                     }
@@ -75,29 +81,57 @@ class EmployeeContainer extends Component {
         }
     };
 
-        filteredEmployees = (searchword) => {
-            if (searchword) {
-                this.setState({
-                    searchFilters: this.state.employees.filter((employee) => {
-                        return (
-                            employee.name.first
+    filteredEmployees = (searchword) => {
+        if (searchword) {
+            this.setState({
+                searchFilters: this.state.employees.filter((employee) => {
+                    return (
+                        employee.name.first
                             .toLowerCase()
                             .contact("", employee.name.last.toLowerCase())
-                            .includes(input) ||
-                            employee.phone.includes(input) ||
-                            employee.phone.replace(/[^\w\s]/gi, "").includes(seachword) ||
-                            employee.email.includes(searchword) ||
-                            this.formatDate(employee.dob.date).includes(searchword)
-                        );
-                        
-                    })
-                })
-            }
+                            .includes(searchword) ||
+                        employee.phone.includes(searchword) ||
+                        employee.phone.replace(/[^\w\s]/gi, "").includes(searchword) ||
+                        employee.email.includes(searchword) ||
+                        this.formatDate(employee.dob.date).includes(searchword)
+                    );
+
+                }),
+            });
+        } else {
+            this.setState({ searchFilters: this.state.employees });
         }
+    };
+
+    formatDate = (date) => {
+        date = new Date(date);
+        let dob = [];
+        dob.push(("0" + (date.getMonth() + 1)).slice(-2));
+        dob.push(("0" + date.getDate()).slice(-2));
+        dob.push(date.getFullYear());
+
+        return dob.join("-");
+    };
 
 
     render() {
-        return (  );
+        return (
+            <>
+                <SearchBar
+                    value={this.state.search}
+                    handleInputChange={this.handleInputChange}
+                    handleFormsubmit={this.handleFormSubmit}
+                />
+                <div className="comntainer mt-3">
+                    <EmployeeTable
+                        state={this.state}
+                        sortBy={this.sortBy}
+                        searchFilters={this.searchFilters}
+                        formatDate={this.formatDate}
+                    />
+                </div>
+            </>
+        );
     }
 }
 
