80d7d5a29aaba41bdf48ec9a4f91e4df3a460578
Arash Saeedi <saeediarash84@gmail.com>
2021-04-30 23:31:44 -0400

    added search bar html, css, and did the header comp. and started the Employee table

---
+++ employee-directory/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./stlye.css";
+
+const Header = () => {
+    return (
+        <header>
+        <h1 className="text-center">Employee Directory</h1>
+        <p className="text-center">
+            Filter throught the headers or by using the search box
+        </p>
+        </header>
+    );
+};
+
+export default Header;
